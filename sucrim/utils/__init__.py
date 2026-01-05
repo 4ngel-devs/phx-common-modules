74f68da990ef3ff7045e95c3226cb49a12db08d0
Manuel Ruiz <manuel.ruiz@gmail.com>
2026-01-05 13:03:05 -0600

    fix: add missing utils __init__.py file
    - Add utils/__init__.py to export DateUtils, SortUtils, and PaginationUtils

---
+++ sucrim/utils/__init__.py
@@ -0,0 +1,7 @@
+"""Utility modules for common operations."""
+
+from .date_utils import DateUtils
+from .pagination_utils import PaginationUtils
+from .sort_utils import SortUtils
+
+__all__ = ["DateUtils", "SortUtils", "PaginationUtils"]
